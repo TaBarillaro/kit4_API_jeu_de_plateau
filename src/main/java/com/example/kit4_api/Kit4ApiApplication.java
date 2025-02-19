7a4d4f4e5658967d705dbeef488e219d05513ac4
TaBarillaro <tania.barillaro@le-campus-numerique.fr>
2025-02-19 15:35:02 +0100

    first commit

---
+++ src/main/java/com/example/kit4_api/Kit4ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kit4_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kit4ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kit4ApiApplication.class, args);
+	}
+
+}
