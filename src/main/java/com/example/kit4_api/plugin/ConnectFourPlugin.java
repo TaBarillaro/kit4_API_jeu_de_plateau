ba658e7beed9adad2a2686dedb19be265f444186
TaBarillaro <tania.barillaro@le-campus-numerique.fr>
2025-02-27 14:40:21 +0100

    create ConnectFourPlugin

--- src/main/java/com/example/kit4_api/plugin/ConnectFourPlugin.java
+++ src/main/java/com/example/kit4_api/plugin/ConnectFourPlugin.java
@@ -1,7 +1,7 @@
 package com.example.kit4_api.plugin;
 
 import fr.le_campus_numerique.square_games.engine.Game;
-import fr.le_campus_numerique.square_games.engine.tictactoe.TicTacToeGameFactory;
+import fr.le_campus_numerique.square_games.engine.connectfour.ConnectFourGameFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
@@ -10,31 +10,30 @@ import java.util.Locale;
 import java.util.Optional;
 
 @Component
-public class TicTacToePlugin implements GamePlugin {
+public class ConnectFourPlugin implements GamePlugin {
 
-    private final TicTacToeGameFactory ticTacToeGameFactory;
+    private final ConnectFourGameFactory connectFourGameFactory;
     private final MessageSource messageSource;
 
-    // constructeur
-    public TicTacToePlugin(MessageSource messageSource) {
-        this.ticTacToeGameFactory = new TicTacToeGameFactory();
+    public ConnectFourPlugin(MessageSource messageSource) {
+        this.connectFourGameFactory = new ConnectFourGameFactory();
         this.messageSource = messageSource;
     }
 
-    @Value("${game.tictactoe.default-player-count:2}")
+    @Value("${game.connectfour.default-player-count:2}")
     private int defaultPlayerCount;
 
-    @Value("${game.tictactoe.default-board-size:3}")
+    @Value("${game.connectfour.default-board-size:7}")
     private int defaultBoardSize;
 
     @Override
     public String getName(Locale locale) {
-        return messageSource.getMessage("game.tictactoe.name", null, locale);
+        return messageSource.getMessage("game.connectfour.name", null, locale);
     }
 
     @Override
     public Game createGame(Optional<Integer> playerCount, Optional<Integer> boardSize) {
-        return ticTacToeGameFactory.createGame(
+        return connectFourGameFactory.createGame(
                 playerCount.orElse(defaultPlayerCount),
                 boardSize.orElse(defaultBoardSize)
         );
